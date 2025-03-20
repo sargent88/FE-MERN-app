0980dd2e219dd9ebc3b039a45deb7ec8b03c2b78
Steen G Sargent <ssargent@nerdunited.com>
2025-03-19 22:28:27 -0600

    fix: add initial routes

---
+++ app/places/page.js
@@ -0,0 +1,8 @@
+export default function PlacesPage() {
+  return (
+    <div>
+      <h1>Places</h1>
+      <p>Here are some places to visit</p>
+    </div>
+  );
+}
---
+++ app/users/page.js
@@ -0,0 +1,8 @@
+export default function UsersPage() {
+  return (
+    <div>
+      <h1>Users</h1>
+      <p>Here are some users</p>
+    </div>
+  );
+}
